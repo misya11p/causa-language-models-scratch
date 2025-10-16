7982f79d0cee7f8cd9339617abb452951bdaef47
komiya <90894519+misya11p@users.noreply.github.com>
2025-10-16 13:59:11 +0900

    add: load_model function

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
-from ._utils import get_model
+from ._utils import get_model, load_model
 from ._vanilla_transformer import VanillaTransformer
 from ._gpt2 import GPT2
--- models/_utils.py
+++ models/_utils.py
@@ -12,3 +12,14 @@ def get_model(arch: str, hparams: dict) -> torch.nn.Module:
         case _:
             raise ValueError(f"Model {arch} not recognized")
     return model
+
+
+def load_model(fpath_ckpt, device="cpu"):
+    checkpoint = torch.load(fpath_ckpt, map_location="cpu")
+    arch = checkpoint["config"]["model"]["arch"]
+    hparams = checkpoint["config"]["model"]["hparams"]
+    params = checkpoint["model"]
+    model = get_model(arch, hparams)
+    model.load_state_dict(params)
+    model.to(device)
+    return model
